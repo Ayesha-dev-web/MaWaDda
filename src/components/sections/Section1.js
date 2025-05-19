f129d5c5c9f491bbc6ea86c9479b268abf40572f
Ayesha-dev-web <ayeshaaamir634@gmail.com>
2025-05-19 06:31:23 +0500

    Update Section1.js

--- src/components/sections/Section1.js
+++ src/components/sections/Section1.js
@@ -8,11 +8,11 @@ import { TbStarsFilled } from "react-icons/tb";
 
 const items = [
    {
-        name: 'Connection',
+        name: 'faith',
         icon: <GiLinkedRings />
     },
     {
-        name: 'Strengthing relationship',
+        name: 'Strengthing relation',
         icon: <GiLinkedRings />
     },
     {
@@ -38,7 +38,7 @@ const Section1 = () => {
     
     <div className='flex w-full bg-[white] mt-[-7vh]'>
 
-        <div className="w-[80%] mx-auto grid grid-cols-1 md:grid-cols-6">
+        <div className="w-[90%] mx-auto grid grid-cols-1 md:grid-cols-6">
             {
                 items.map((me, index)=>(
                     <div key={index} className='grid grid-cols-3 md:flex gap-4 md:items-center md:mx-auto p-5 text-blue cursor-pointer'>
